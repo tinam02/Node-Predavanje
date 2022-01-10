6b3f72bf356b399063981e744249ee5c8c131e53
Tina <oge612@gmail.com>
2022-01-10 19:12:14 +0100

    Stream
    reader.on data,end,error

--- fs.js
+++ fs.js
@@ -1,17 +1,37 @@
 const fs = require("fs");
-fs.readFile("fss.txt", "utf-8", (err, data) => {
-  //bez utf-8 pise bitove
-  //err ispali ako postoji greska
-  if (err) {
-    throw err;
-  }
-  console.log("Sadrzaj: " + data);
+// fs.readFile("fss.txt", "utf-8", (err, data) => {
+//   //bez utf-8 pise bitove
+//   //err ispali ako postoji greska
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Sadrzaj: " + data);
+// });
+// //upisivanje u radovan.txt
+// var content = "Zdravo";
+// fs.writeFile("radovan.txt", content, (err) => {
+//   //err ispali ako postoji greska
+//   if (err) {
+//     throw err;
+//   }
+// });
+
+//!Stream;
+var reader = fs.createReadStream("fss.txt");
+reader.setEncoding("utf-8");
+var ispis = "";
+// *Citanje
+reader.on("data", function (string) {
+  //on je listener u nodejsu
+  console.log(`Data:`);
+  ispis += string;
 });
-//upisivanje u radovan.txt
-var content = "Zdravo";
-fs.writeFile("radovan.txt", content, (err) => {
-  //err ispali ako postoji greska
-  if (err) {
-    throw err;
-  }
+//*Zavrsetak citanja
+reader.on("end", function () {
+  console.log(ispis);
 });
+// *Hendlovanje greske
+reader.on("error", function (err) {
+  console.log(err.message); //message ili stack
+});
+
